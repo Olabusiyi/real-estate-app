cce5d79304b7d8a92af59ae453fa946ec6403c4b
Olabusiyi <olabusiyi@gmail.com>
2022-09-07 01:36:24 -0500

    Added gitignore and Procfile

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ app.post("/property", function(req,res){
 
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Server started on port 3000")
+  console.log("Server running successfully")
 });
 
 
